d2e622503455f994d2be676699ee9671ac60cae8
Rave <165758966+RaveZu@users.noreply.github.com>
2025-08-19 21:40:43 +0630

    Update server.py

--- server.py
+++ server.py
@@ -2,15 +2,28 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-# Serve main page
+# Main page
 @app.route('/')
-def serve_index():
+def home():
     return send_from_directory('.', 'home.html')
 
-# Serve any other file in the folder
-@app.route('/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('.', filename)
+# Other pages
+@app.route('/component')
+def component():
+    return send_from_directory('.', 'component.html')
+
+@app.route('/donate')
+def donate():
+    return send_from_directory('.', 'donate.html')
+
+@app.route('/smartcity')
+def smartcity():
+    return send_from_directory('.', 'smartcity.html')
+
+# Serve CSS
+@app.route('/style.css')
+def style():
+    return send_from_directory('.', 'style.css')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
